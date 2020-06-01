af01881f251c3d51bbde8808dcc92904999249fd
amarsomani <amarsomani06@gmail.com>
2020-06-01 12:11:40 +0530

    React Redux Forms Revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap'
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
 const required = val => val && val.length;
@@ -15,7 +15,7 @@ class Contact extends Component {
     handleSubmit = (values) => {
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
-
+        this.props.resetFeedbackForm();
     }
 
 
@@ -67,7 +67,7 @@ class Contact extends Component {
                         <h3>Send us your Feedback</h3>
                     </div>
                     <div className="col-12 col-md-9">
-                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                        <Form model="feedback" onSubmit={values => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -85,7 +85,7 @@ class Contact extends Component {
                                         messages={{
                                             required: 'Required ',
                                             minlength: 'Must be greater than 3 characters',
-                                            maxlength: 'Must be less tha 15 characters'
+                                            maxlength: 'Must be less than 15 characters'
                                         }}
                                     />
                                 </Col>
@@ -194,7 +194,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                 </div>
             </div>
